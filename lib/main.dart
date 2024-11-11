bd11ca300b1238e0c61e2a9317e05488a1d2271d
Oluwaferanmi Sao <caleb4sao@gmail.com>
2024-11-11 02:53:21 +0100

    added theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:result_checker/pages/home_screen.dart';
 
+var kcolorScheme = ColorScheme.fromSeed(
+  seedColor: Color(0xFF2A3663),
+);
+
 void main() {
   // Flutter's run app statement
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData().copyWith(
+        colorScheme: kcolorScheme,
+      ),
       home: HomeScreen(),
     ),
   );
